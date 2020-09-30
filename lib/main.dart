ede883d2a7ccdb564448c1b3797232f158ccb6fb
Jordan Seiler <jordanseiler@jordans-mbp.lan>
2020-09-30 17:44:42 -0500

    Added greeting to homescreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+const Color shockerYellow = const Color(0xFFFFCD00);
 
 void main() {
   runApp(MyApp());
@@ -9,9 +12,21 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.yellow,
+        backgroundColor: shockerYellow,
         body: SafeArea(
-          child: Container(),
+          child: Container(
+            child: Align(
+              alignment: FractionalOffset(0.04, 0.24),
+              child: Text(
+                'Good morning,\nVariable',
+                textDirection: TextDirection.ltr,
+                textAlign: TextAlign.left,
+                style: GoogleFonts.josefinSans(
+                    textStyle:
+                        TextStyle(fontSize: 60, fontWeight: FontWeight.w500)),
+              ),
+            ),
+          ),
         ),
       ),
     );
